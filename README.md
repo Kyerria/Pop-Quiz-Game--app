7930d335951e2e6abed18cdf0fa3d49cc44d3e1a
mjackson29 <kmj61221@gmail.com>
2024-03-02 11:59:59 -0600

    commit

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
-# Download-your-brain!-app
-The question are from the Open Triva API https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple. Which is where I fetched my question from in caterogory comouter:science. 
-
-The questions are then presented one by one. 5 questions in total with four answer choices A,B,C and D. Once it reaches the end of questions the game is over. 
+# mini-pop-quiz game!-app
 
 
+## Project Choice (Tell us which project you're doing!)
+Pop-Quiz Game
+## Project Description
+>This is a pop quiz that generate questions from an triva API. The
+questions are medium difficult, catefory is computer Science and multiple choice. There are only 5 questions. If you wish to play over and over again the questions not repeat and will be different. 
 
+
+## 3rd Party API Link & Proof of Concept
+> API: https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple
+
+
+
+## Wire Frames
+> Copy and paste or drag and drop your images here.
+
+## User Stories
+> Add user stories following the _As a [type of user], I want [what the user wants], so that [what it helps accomplish]_ format.
+
+I want the user to leave 
+### MVP Goals
+> What is the minimum functionality you commit to meet to pass this project?
+The minimum funtionality for this quiz is to have my css.style working and buttons functioning correctly.
+
+
+### Stretch Goals
+> 
+My goal is to have the pointboard working so the that user can keep up with his points per question.
--- play.js
+++ play.js
@@ -16,11 +16,17 @@ function startGame(){
 }
 
 function nextUpdate(choices) {
+    // updates the choices for the current question 
     choiceButtons.forEach((button, index) => {
+        // updates each the answers content of each choice button
         button.textContent = choices['choice' + (index + 1)];
+        //object is choice. array index starts at 0. so choice + 0+1= choice1
         button.addEventListener('click', function (){
-            const selectedChoice = choices[index].textContent;
+            //the event is click and will be executed when button is clicked.
+            const selectedChoice = button.textContent;
+            //takes the text of the choice to the current button
             const correctChoice = currentQuestion.correct_answer;
+            //to get the correct answer for the current question
             if(selectedChoice === correctChoice) {
                 button.classList.add('correct-answer');
                 console.log('Correct!');
@@ -34,6 +40,7 @@ fetch('https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=m
 // fetched the questions from my open triva api and paste it
 .then(response => {
 if(response.status === 429){
+    //Code 5: Rate Limit Too many requests have occurred. Each IP can only access the API once every 5 seconds.
     console.log('Rate limit exceeded. Waiting for 5 seconds...');
     return new Promise(resolve => setTimeout(resolve, 5000))
     .then(fetchQuestions);
@@ -41,26 +48,30 @@ if(response.status === 429){
 return response.json();
 })
 
-.then(loadedQuestions => {
-    console.log(loadedQuestions);
-    const formattedQuestions = loadedQuestions.results.map(loadedQuestion => {
+.then(fixedQuestions => {
+    console.log(fixedQuestions);
+    const formattedQuestions = fixedQuestions.results.map(fixedQuestion => {
+        //Array of formmated questions by mapping over each question
         const formattedQuestion = {
-            question: loadedQuestion.question,
-            correct_answer: loadedQuestion.correct_answer,
-            answers: [...loadedQuestion.incorrect_answers, loadedQuestion.correct_answer]
+            question: fixedQuestion.question,
+            correct_answer: fixedQuestion.correct_answer,
+            answers: [...fixedQuestion.incorrect_answers, fixedQuestion.correct_answer]
         };
     return formattedQuestion;
     });
 
   formattedQuestions.forEach(question => {
     question.choices = {};
+    //go through each answer in the array of questions.
     question.answers.forEach((choice, index) => {
+        //assign answer to the choice 1 , 2 ,3, and 4.
         question.choices['choice' + (index + 1)] = choice;
     });
   });
  
   console.log(formattedQuestions);
   startGame();
+
   nextUpdate(formattedQuestions[0].choices);
 
   
@@ -71,12 +82,14 @@ return response.json();
 })
 
 .catch (error => {
+    //catches any error from the API and logs them in to the console.
     console.log('error occured:',error);
 });
 
 const Max_Questions = 5;
 
 const nextQuestion = () => {
+    //display the next question after clicking next button up to 5 then the game is over.
     if(availableQuestions.length === 0 || questionNumber >= 5) { 
 console.log("Game Over");
 return;
@@ -84,19 +97,24 @@ return;
 
 questionNumber++;
 const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+//get an randon question by rounding down to the nearest interger.
+//question index range from 0 number to 4 number. 
 currentQuestion = availableQuestions[questionIndex];
 questionElement.textContent = currentQuestion.question;
+//connect to the html to represent the current question.
 console.log("Question:", currentQuestion.question);
 console.log("choices:", currentQuestion.choices);
 
 nextUpdate(currentQuestion.choices);
 
 availableQuestions.splice(questionIndex, 1);
-
+//this function stops the questions from repeating in the rounds of the game.
+//splice remove or replace existing or new element. 
 
 };
 
 document.querySelector('.button').addEventListener('click', nextQuestion);
+//Adds an event to the next button when clicked on
 
 
 
